b3142e955408cbcd1b499961bf69870bd196d0ab
athanase-galatsidas <athanase.galatsidas@gmail.com>
2021-09-29 21:16:32 +0200

    setup frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue';
+</script>
+
+<template>
+	<AppHeader title="Sand Witches eSandwiches Service&trade;" />
+</template>
+
+<style lang="postcss">
+html {
+	@apply bg-gray-100 dark:bg-gray-800;
+}
+</style>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'AppHeader',
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+	},
+	setup() {},
+});
+</script>
+
+<template>
+	<header class="bg-red-400 p-4">
+		<h1 class="text-2xl">{{ title }}</h1>
+	</header>
+</template>
