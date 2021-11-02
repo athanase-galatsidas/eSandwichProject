a07e375925b6611df11e6b1f642e29c81715d8b0
athanase-galatsidas <athanase.galatsidas@gmail.com>
2021-11-02 15:53:03 +0100

    basic login page

--- frontend/src/screens/Login.vue
+++ frontend/src/screens/Login.vue
@@ -1,8 +1,10 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { UserIcon } from '@heroicons/vue/outline';
+import { UserIcon, MailIcon, LockClosedIcon, KeyIcon } from '@heroicons/vue/outline';
+import InputGroup from '../components/InputGroup.vue';
 
 export default defineComponent({
+	name: 'Login',
 	props: {
 		signup: {
 			type: Boolean,
@@ -17,7 +19,11 @@ export default defineComponent({
 		};
 	},
 	components: {
+		InputGroup,
 		UserIcon,
+		MailIcon,
+		LockClosedIcon,
+		KeyIcon,
 	},
 	methods: {
 		toggleSignin(value: boolean) {
@@ -29,6 +35,7 @@ export default defineComponent({
 
 <template>
 	<form
+		v-if="showSignUp"
 		class="
 			flex flex-col
 			justify-center
@@ -41,34 +48,90 @@ export default defineComponent({
 			mt-16
 		"
 	>
-		<h3 class="text-2xl mb-2" v-if="showSignUp">Sign up</h3>
-		<h3 class="text-2xl mb-2" v-else>Log In</h3>
+		<h3 class="text-2xl mb-2">Sign up</h3>
 
-		<div class="relative my-2">
-			<input
-				id="email"
-				type="text"
-				placeholder=" "
-				class="hide-on-input p-2 h-9 w-64 bg-gray-100 rounded-md shadow-sm"
-			/>
-			<label
-				for="email"
-				class="absolute h-9 px-2 left-0 top-0 flex items-center text-gray-500 select-none"
-			>
-				<UserIcon class="h-6 w-6 mr-1" /> email
-			</label>
-		</div>
+		<InputGroup id="username" text="Username">
+			<UserIcon class="h-6 w-6 mr-2" />
+		</InputGroup>
 
-		<input v-if="showSignUp" type="submit" value="Sign Up" />
-		<input v-else type="submit" value="Log in" />
+		<InputGroup id="email" text="E-mail Adress" type="email">
+			<MailIcon class="h-6 w-6 mr-2" />
+		</InputGroup>
 
-		<p v-if="showSignUp" class="cursor-default">
+		<InputGroup id="password" text="Password" type="password">
+			<KeyIcon class="h-6 w-6 mr-2" />
+		</InputGroup>
+
+		<InputGroup id="password-2" text="Repeat Password" type="password">
+			<KeyIcon class="h-6 w-6 mr-2" />
+		</InputGroup>
+
+		<input
+			class="
+				hide-on-input
+				my-2
+				mt-4
+				p-2
+				h-9
+				w-64
+				bg-red-500
+				text-white
+				font-semibold
+				rounded-md
+				shadow-sm
+			"
+			type="submit"
+			value="Sign Up"
+		/>
+
+		<p class="cursor-default">
 			Already have an account?
 			<span class="cursor-pointer text-red-600 font-semibold" @click="toggleSignin(false)">
 				Log in here!
 			</span>
 		</p>
-		<p v-else class="cursor-default">
+	</form>
+	<form
+		v-else
+		class="
+			flex flex-col
+			justify-center
+			items-center
+			p-4
+			rounded-md
+			mx-auto
+			max-w-screen-sm
+			bg-white
+			mt-16
+		"
+	>
+		<InputGroup id="username" text="Email / Username">
+			<UserIcon class="h-6 w-6 mr-2" />
+		</InputGroup>
+
+		<InputGroup id="password" text="Password" type="password">
+			<LockClosedIcon class="h-6 w-6 mr-2" />
+		</InputGroup>
+
+		<input
+			class="
+				hide-on-input
+				my-2
+				mt-4
+				p-2
+				h-9
+				w-64
+				bg-red-500
+				text-white
+				font-semibold
+				rounded-md
+				shadow-sm
+			"
+			type="submit"
+			value="Log In"
+		/>
+
+		<p class="cursor-default">
 			Don't have an account?
 			<span class="cursor-pointer text-red-600 font-semibold" @click="toggleSignin(true)">
 				Sign up now!
@@ -76,16 +139,3 @@ export default defineComponent({
 		</p>
 	</form>
 </template>
-
-<style scoped>
-/* 
-this hides the label after the input when its filled in
-used to hide icon & placeholder label
-important: for this to work the input must be placed before the label and it must have a placeholder text " "
-*/
-.hide-on-input:active + label,
-.hide-on-input:focus + label,
-.hide-on-input:not(:placeholder-shown) + label {
-	opacity: 0; /* using anything other than opacity to hide this causes weird behaviour */
-}
-</style>
