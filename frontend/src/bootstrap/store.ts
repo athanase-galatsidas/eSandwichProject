5ce4f9e0f1b692eacc6bddad01b6f713a0c4b17c
athanase-galatsdias <athanase.galatsidas@gmail.com>
2021-11-19 18:08:19 +0100

    fixed overlay

--- frontend/src/bootstrap/store.ts
+++ frontend/src/bootstrap/store.ts
@@ -1,16 +1,21 @@
 import { createStore } from 'vuex';
 import { get, post } from '@/modules/network';
+import { Sandwich } from '@/interfaces/Sandwich';
 
 const url = 'http://localhost:3001/v1';
 
 export default createStore({
 	state: {
-		sandwitches: [],
+		sandwitches: Array<Sandwich>(),
+		cart: Array<Sandwich>(),
 	},
 
 	mutations: {
 		setData(state, payload: any) {
 			state.sandwitches = payload;
+		},
+		addCartItem(state, payload: Sandwich) {
+			state.cart.push(payload);
 		},
 	},
 
@@ -20,7 +25,8 @@ export default createStore({
 				.then((res) => res.json())
 				.then((data) => {
 					// TODO: doe iets met data
-					this.commit('setData', data.sandwiches);
+					const sandwiches = data.sandwiches.map((res: Object) => res as Sandwich);
+					this.commit('setData', sandwiches);
 				});
 		},
 	},
