9f3615188e79e6d2559a879d193973f4baf7c9cc
athanase-galatsidas <athanase.galatsidas@gmail.com>
2021-11-22 15:51:54 +0100

    switched to mysql database

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -11,111 +11,78 @@ import { createDatabase } from 'typeorm-extension';
 import { Sandwich } from './entities/sandwich';
 import { seedDatabase } from './seeders/dataSeeder';
 
-// APP
-const app = express(),
-	port = process.env.PORT || 3001;
-const url = `http://localhost:${port}`;
+(async () => {
+	const connectionOptions: ConnectionOptions = await getConnectionOptions();
 
-// MIDDLEWARE
-app.use(express.json());
-app.use(cors())
-app.use('/img', express.static(`${__dirname}/assets/images`));
+	createDatabase({ ifNotExist: true }, connectionOptions)
+		.then(() => console.log('db created'))
+		.then(
+			async () =>
+				await createConnection().then(async (connection: Connection) => {
+					// APP
+					const app = express(),
+						port = process.env.PORT || 3001;
+					const url = `http://localhost:${port}`;
 
-// ROUTES
-app.get('/', (request: Request, response: Response) => {
-	response.send(`Welcome to Sandwitches Sandwich Service!`);
-});
+					// MIDDLEWARE
+					app.use(express.json());
+					app.use(cors());
+					app.use('/img', express.static(`${__dirname}/assets/images`));
 
-// TODO: temporary oplosing tot database trug werkt
-app.get('/v1/sandwiches', (request: Request, response: Response) => {
-	const data = {
-		sandwiches: [
-			{
-				id: 0,
-				name: 'Basic Sandwich',
-				image: `${url}/img/basic.webp`,
-				price: 11.5,
-			},
-			{
-				id: 1,
-				name: 'Regular Sandwich',
-				image: `${url}/img/regular.webp`,
-				price: 12.0,
-			},
-			{
-				id: 2,
-				name: 'Vegetarian Sandwich',
-				image: `${url}/img/vege.webp`,
-				price: 13.0,
-			},
-			{
-				id: 3,
-				name: 'Special Sandwich',
-				image: `${url}/img/special.webp`,
-				price: 14.0,
-			},
-			{
-				id: 4,
-				name: 'Grilled Sandwich',
-				image: `${url}/img/grilled.webp`,
-				price: 12.0,
-			},
-			{
-				id: 5,
-				name: 'Deluxe Sandwich',
-				image: `${url}/img/deluxe.webp`,
-				price: 14.0,
-			},
-		],
-	};
-	response.send(JSON.stringify(data));
+					// ROUTES
+					app.get('/', (request: Request, response: Response) => {
+						response.send(`Welcome to Sandwitches Sandwich Service!`);
+					});
 
-});
+					// TODO: temporary oplosing tot database trug werkt
+					app.get('/v1/sandwiches', (request: Request, response: Response) => {
+						const data = {
+							sandwiches: [
+								{
+									id: 0,
+									name: 'Basic Sandwich',
+									image: `${url}/img/basic.webp`,
+									price: 11.5,
+								},
+								{
+									id: 1,
+									name: 'Regular Sandwich',
+									image: `${url}/img/regular.webp`,
+									price: 12.0,
+								},
+								{
+									id: 2,
+									name: 'Vegetarian Sandwich',
+									image: `${url}/img/vege.webp`,
+									price: 13.0,
+								},
+								{
+									id: 3,
+									name: 'Special Sandwich',
+									image: `${url}/img/special.webp`,
+									price: 14.0,
+								},
+								{
+									id: 4,
+									name: 'Grilled Sandwich',
+									image: `${url}/img/grilled.webp`,
+									price: 12.0,
+								},
+								{
+									id: 5,
+									name: 'Deluxe Sandwich',
+									image: `${url}/img/deluxe.webp`,
+									price: 14.0,
+								},
+							],
+						};
+						response.send(JSON.stringify(data));
+					});
 
-// START
-app.listen(port, () => {
-	console.info(`\nServer listening on ${url}/`);
-});
-
-
-
-// TODO: fix de database
-
-// (async () => {
-// 	const connectionOptions: ConnectionOptions = await getConnectionOptions();
-
-// 	// Create the database before we make the connection
-// 	createDatabase({ ifNotExist: true }, connectionOptions)
-// 		.then(() => console.log('database created'))
-// 		.then(createConnection)
-// 		.then(async (connection: Connection) => {
-// 			// seed database
-// 			seedDatabase(connection);
-
-// 			// APP SETUP
-// 			const app = express(),
-// 				port = process.env.PORT || 3001;
-
-// 			// MIDDLEWARE
-// 			app.use(express.json());
-//			app.use(express.static('assets'));
-
-// 			// ROUTES
-// 			app.get('/', (request: Request, response: Response) => {
-// 				response.send(`Welcome to Sandwitches Sandwich Service!`);
-// 			});
-
-// 			// app.get('/v1/sandwich', async (request: Request, response: Response) => {
-// 			// 	// quick test
-// 			// 	// todo: move this to sandwich controller
-// 			// 	const data = await getRepository(Sandwich).find();
-// 			// 	response.send(JSON.stringify(data));
-// 			// });
-
-// 			// APP START
-// 			app.listen(port, () => {
-//				console.info(`\nServer listening on ${url}/`);
-// 			});
-// 		})
-// 		.catch((error) => console.error(error));
-// })();
+					// START
+					app.listen(port, () => {
+						console.info(`\nServer listening on ${url}/`);
+					});
+				}),
+		);
+})();
