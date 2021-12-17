131b46e92d4d7c59cb0feb298514398b503e1902
athanase-galatsidas <athanase.galatsidas@gmail.com>
2021-12-17 20:17:55 +0100

    added pwa support

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -1,9 +1,10 @@
-import { precacheAndRoute } from 'workbox-precaching';
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
 
 declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener('message', (event) => {
 	if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
-// self.__WB_MANIFEST is default injection point
+
+cleanupOutdatedCaches();
 precacheAndRoute(self.__WB_MANIFEST);
