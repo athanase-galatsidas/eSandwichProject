5574f0ddd6362485523fcf3136c6b9aaf9163924
Tibo Verbeke <tibo.verbeke@student.howest.be>
2022-08-18 15:42:38 +0200

    trying with config file

---
+++ frontend/src/config.ts
@@ -0,0 +1,7 @@
+window._env_ = {
+    apiKey: 'AIzaSyCvDZHwAQY2baYAircGfGgQNPBQsJDrhWc',
+    authDomain: 'sandwitchservice.firebaseapp.com',
+    projectId: 'sandwitchservice',
+    storageBucket: 'sandwitchservice.appspot.com',
+    messagingSenderId: '307541290120',
+};
